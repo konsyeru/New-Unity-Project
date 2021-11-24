79b993f912833d2ec5e2df4ddaf8496e4ab97c2f
B-Beans <74563485+B-Beans@users.noreply.github.com>
2021-11-24 21:49:59 +0900

    ConfigrationSeneの値が保持できるようにした

--- Assets/Scripts/ConfigrationSean/ContentInformation.cs
+++ Assets/Scripts/ConfigrationSean/ContentInformation.cs
@@ -8,10 +8,10 @@ public class ContentInformation : MonoBehaviour
 {
     GameObject Content;
     public int ChildCount;
+    static public int[] ChildCounts = {0, 0, 0, 0, 0, 0}; 
     public GameObject[] ChildObject;
-
-    static public int[,] TimeSS, TimeSP, TimeHL, TimeSL, TimeFR;
-    static public float[] IntensitySS, IntensitySP, IntensityHL, IntensitySL, IntensityFR;
+    static public int[][][] Time = new int[5][][];  //TimeSS, TimeSP, TimeHL, TimeSL, TimeFR;
+    static public float[][] Intensity = new float[5][]; //IntensitySS, IntensitySP, IntensityHL, IntensitySL, IntensityFR;
     static public int[] ColorHL;
 
     // Start is called before the first frame update
@@ -24,66 +24,44 @@ public class ContentInformation : MonoBehaviour
 
         ChildCount = Content.transform.childCount;  //Contentの子オブジェクトの要素数を取得
         ChildObject = new GameObject[ChildCount];  //Content子オブジェクトを格納するための配列
+        ChildCounts[LightChange.DropdownValue] = ChildCount;
 
         for(int i = 0; i < ChildCount; i++){  //ChiledObjectにObjectを格納
             ChildObject[i] = Content.transform.GetChild(i).gameObject;
         }
 
         //配列の要素数の初期化
-        if(LightChange.DropdownValue == 0){  //SS
-            TimeSS = new int[ChildCount , 2];
-            IntensitySS = new float[ChildCount];
-        }else if(LightChange.DropdownValue == 1){  //SP
-            TimeSP = new int[ChildCount , 2];
-            IntensitySP = new float[ChildCount];
-        }else if(LightChange.DropdownValue == 3){  //HL
-            TimeHL = new int[ChildCount , 2];
-            IntensityHL = new float[ChildCount];
+        Time[LightChange.DropdownValue] = new int[ChildCount][];
+        Intensity[LightChange.DropdownValue] = new float[ChildCount];
+        ChildCounts[LightChange.DropdownValue] = ChildCount;
+        if(LightChange.DropdownValue == 3){  //HL
             ColorHL = new int[ChildCount];
-        }else if(LightChange.DropdownValue == 4){  //SL
-            TimeSL = new int[ChildCount , 2];
-            IntensitySL = new float[ChildCount];
-        }else if(LightChange.DropdownValue == 5){  //FR
-            TimeFR = new int[ChildCount , 2];
-            IntensityFR = new float[ChildCount];
         }
 
     }
 
-
-    void SaveInformation(int[,] time, float[] intensity, bool color=false){  //配列に情報を保存
+    void SaveInformation(){  //配列に情報を保存
         for(int i = 0; i < ChildCount; i++){
             //Time:秒に直して保存
             int start_minute = int.Parse(ChildObject[i].transform.Find("InputField1-1").gameObject.GetComponent<TMP_InputField>().text);
             int start_second = int.Parse(ChildObject[i].transform.Find("InputField1-2").gameObject.GetComponent<TMP_InputField>().text);
             int end_minute = int.Parse(ChildObject[i].transform.Find("InputField2-1").gameObject.GetComponent<TMP_InputField>().text);
             int end_second = int.Parse(ChildObject[i].transform.Find("InputField2-2").gameObject.GetComponent<TMP_InputField>().text);
-            time[i, 0] = start_minute*60+start_second;
-            time[i, 1] = end_minute*60+end_second;
+            Time[LightChange.DropdownValue][i] = new int[2];
+            Time[LightChange.DropdownValue][i][0] = start_minute * 60 + start_second; 
+            Time[LightChange.DropdownValue][i][1] = end_minute * 60 + end_second;
             //Intensity
-            intensity[i] = ChildObject[i].transform.Find("Slider").gameObject.GetComponent<Slider>().value;
+            Intensity[LightChange.DropdownValue][i] = ChildObject[i].transform.Find("Slider").gameObject.GetComponent<Slider>().value;
             //color
-            if(color){
+            if(LightChange.DropdownValue == 3){
                 ColorHL[i] = ChildObject[i].transform.Find("Dropdown").gameObject.GetComponent<TMP_Dropdown>().value;
             }
         }
     }
 
     public void Click(){
-
-        ArrayPreparation(); //配列を用意
-
-        if(LightChange.DropdownValue == 0){        //SS
-            SaveInformation(TimeSS, IntensitySS);
-        }else if(LightChange.DropdownValue == 1){  //SP
-            SaveInformation(TimeSP, IntensitySP);
-        }else if(LightChange.DropdownValue == 3){  //HL
-            SaveInformation(TimeHL, IntensityHL, true);
-        }else if(LightChange.DropdownValue == 4){  //SL
-            SaveInformation(TimeSL, IntensitySL);
-        }else if(LightChange.DropdownValue == 5){  //FR
-            SaveInformation(TimeFR, IntensityFR);
-        }        
+        ArrayPreparation();  //配列を用意
+        SaveInformation();  //配列に情報を保存
     }
 
 }
